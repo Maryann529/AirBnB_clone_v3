00a9f71836842a610e7a34892586d75d66d1a189
Maryann Unoarumi <maryann.pclng@gmail.com>
2022-10-25 00:03:46 +0100

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-"""
-initializes the api views from a flask blueprint set up in api/v1/app.py
-"""
-
-from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
+"""init file"""
+from flask import Blueprint, render_template, abort
+app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
-from api.v1.views.places import *
 from api.v1.views.places_reviews import *
-from api.v1.views.places_amenities import *
